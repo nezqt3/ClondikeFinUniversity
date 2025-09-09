7d0dcdb5335eb7108b1f3ed7e4473aff5ae5f1b4
Denis <dalekseenko8@gmail.com>
2025-09-09 14:31:47 +0300

    change cicle

--- clondike_game.py
+++ clondike_game.py
@@ -3,7 +3,7 @@ from string import printable
 def print_matrix(matrix):
     print(" ", *printable[10:20])
     for i in range(len(matrix)):
-        print(i, *matrix[i])
+        print(i + 1, *matrix[i])
         
 def create_matrix():
     return [[0] * 10 for _ in range(10)]
