aef1cc8beb544471de221695d68d2d8f135bb54a
Denis <dalekseenko8@gmail.com>
2025-09-09 14:21:19 +0300

    print matrix

--- clondike_game.py
+++ clondike_game.py
@@ -1,5 +1,13 @@
+from string import printable
+
 def print_matrix(matrix):
+    print(" ", *printable[10:20])
     for i in range(len(matrix)):
-        print(i + 1, *matrix[i])
+        print(i, *matrix[i])
         
-print_matrix([[0, 0], [0, 0], [0, 0]])
+def create_matrix():
+    return [[0] * 10 for _ in range(10)]
+        
+matrix = create_matrix()
+
+print_matrix(matrix)
