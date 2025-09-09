0f881503ce9f4ddf079f1ecdfd9a90a669d2083e
rudactor <ilya124ver3@gmail.com>
2025-09-09 15:33:04 +0300

    added ask for move function

--- clondike_game.py
+++ clondike_game.py
@@ -7,7 +7,18 @@ def print_matrix(matrix):
         
 def create_matrix():
     return [[0] * 10 for _ in range(10)]
-        
+
+def ask_for_move():
+    move = input("Сделайте следующий ход: ")
+    letter = "".join([ch for ch in move if ch.isalpha()])
+    digit = int("".join([ch for ch in move if ch.isdigit()]))
+
+    if (not letter or not digit) or (letter < "a" or letter > "j") or (digit < 1 or digit > 10): # нужно добавить проверку занята ли уже клетка которую выбирает игрок
+        print("Вы ввели неправильные координаты. Попробуйте еще раз.")
+        ask_for_move()
+    else:
+        return digit, letter
+    
 matrix = create_matrix()
 
 print_matrix(matrix)
